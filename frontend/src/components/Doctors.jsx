79ee15b8c2f5511ddde8a3400f510f75fd7e2d4e
xlwv <pkomaldeep@gmail.com>
2025-02-11 20:20:15 +0530

    decond

--- frontend/src/components/Doctors.jsx
+++ frontend/src/components/Doctors.jsx
@@ -14,25 +14,25 @@ function Doctors() {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 <div className='z-15 p-6 justify-center gap-3 flex items-center flex-col bg-[linear-gradient(#F7E1DA_10%,white_100%)]  rounded-3xl border-3 border-[#EEEEEE]'>
 
-<div className='w-45 h-45 rounded-full bg-white'>
+                    <div className='w-45 h-45 rounded-full bg-white'>
 
-</div>
-                    <h4 className='font-bold text-5xl'>Dr. Arun Kumar Balakrishnan</h4>
-<div className='text-center'>
-                    <span>M.Ch.(Urology), FRCS (EDIN), M.S.(Gen.Surgery), MBBS</span>
-                    <p>Managing Director, Chief Consultant Urologist
-                        Robotic surgeon and Uro Oncologist</p>
-</div>
+                    </div>
+                    <h4 className='font-bold text-5xl text-center'>Dr. Arun Kumar Balakrishnan</h4>
+                    <div className='text-center  justify-center'>
+                        <span>M.Ch.(Urology), FRCS (EDIN), M.S.(Gen.Surgery), MBBS</span>
+                        <p>Managing Director, Chief Consultant Urologist
+                            Robotic surgeon and Uro Oncologist</p>
+                    </div>
                 </div>
                 <div className=' z-15 p-8 justify-center items-center gap-3 flex flex-col bg-[linear-gradient(#F7E1DA_10%,white_100%)]  rounded-3xl border-3 border-[#EEEEEE]'>
 
 
-                  
-<div className='w-45 h-45 rounded-full bg-white'></div>
-                    <h4 className='font-bold text-xl'>Dr. D. Venkat Subramaniam
+
+                    <div className='w-45 h-45 rounded-full bg-white'></div>
+                    <h4 className='font-bold text-xl text-center'>Dr. D. Venkat Subramaniam
                     </h4>
-                    <div className='text-center'>
-                        <span>
+                    <div className='text-center  justify-center'>
+                        <span className='text-center'>
                             M.S, D.Lap, M.Ch(Uro)
                         </span>
                         <p>Executive Director and Senior Consultant Urologist</p>
@@ -42,18 +42,18 @@ function Doctors() {
                 <div className='  z-15 p-8 justify-center items-center gap-3 flex flex-col bg-[linear-gradient(#F7E1DA_10%,white_100%)]  rounded-3xl border-3 border-[#EEEEEE]'>
 
 
-                 
-<div className='w-45 h-45 rounded-full bg-white'></div>
-                    <h4 className='font-bold text-xl'>Dr. Navinath M</h4>
-                    <div className='text-center'>
-                    <span>MBBS, MD (Med) (AIIMS), DM (Nephro) (AIIMS),
 
-                        DNB (Nephro), MNAMS</span>
-                    <p>Consultant Nephrologist</p>
+                    <div className='w-45 h-45 rounded-full bg-white'></div>
+                    <h4 className='font-bold text-xl text-cente'>Dr. Navinath M</h4>
+                    <div className='text-center  justify-center'>
+                        <span>MBBS, MD (Med) (AIIMS), DM (Nephro) (AIIMS),
+
+                            DNB (Nephro), MNAMS</span>
+                        <p>Consultant Nephrologist</p>
                     </div>
 
                 </div>
-                
+
             </div>
 
 
